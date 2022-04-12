403e1f82c4d23525e86904ed122da67661e9be64
Arif Widayana <arifwidayana@gmail.com>
2022-04-12 01:41:34 -0700

    new features CRUD

---
+++ app/src/main/java/com/arifwidayana/challengechapter4/view/homepage/AddStocksFragment.kt
@@ -0,0 +1,84 @@
+package com.arifwidayana.challengechapter4.view.homepage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.arifwidayana.challengechapter4.databinding.FragmentAddStocksBinding
+import com.arifwidayana.challengechapter4.model.StocksDatabase
+import com.arifwidayana.challengechapter4.model.database.StocksEntity
+import com.arifwidayana.challengechapter4.model.utils.Constant
+import com.arifwidayana.challengechapter4.model.utils.SharedPreference
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+
+@DelicateCoroutinesApi
+class AddStocksFragment : DialogFragment(){
+    private var bind : FragmentAddStocksBinding? = null
+    private val binding get() = bind!!
+    private var dataStocks : StocksDatabase? = null
+    private lateinit var shared : SharedPreference
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        bind = FragmentAddStocksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        shared = SharedPreference(requireContext())
+        dataStocks = StocksDatabase.getData(requireContext())
+
+        binding.apply {
+            btnAddStocks.setOnClickListener {
+                val codeStocks = etCodeStocks.text.toString()
+                val nameStocks = etNameStocks.text.toString()
+                val equityStocks = etEquityStocks.text.toString().toInt()
+                val netProfitStocks = etNetProfitStocks.text.toString().toInt()
+                val priceStocks = etPriceShareStocks.text.toString().toInt()
+                val sharesStocks = etShareStocks.text.toString().toInt()
+                val bvStocks = equityStocks/sharesStocks
+                val pbvStocks = priceStocks/bvStocks.toDouble()
+                val epsStocks = netProfitStocks.toDouble()/sharesStocks
+                val user = shared.getString(Constant.USER)
+
+                val objStocks = StocksEntity(
+                    null,
+                    codeStocks,
+                    nameStocks,
+                    equityStocks,
+                    netProfitStocks,
+                    pbvStocks,
+                    epsStocks,
+                    priceStocks,
+                    sharesStocks,
+                    user
+                )
+
+                GlobalScope.async {
+                    dataStocks?.stocksDao()?.insertDataStocks(objStocks)
+                }
+                Toast.makeText(requireContext(), "Add Stocks Success", Toast.LENGTH_SHORT).show()
+                dialog?.dismiss()
+            }
+
+            btnCancelSave.setOnClickListener {
+                Toast.makeText(requireContext(), "You have canceled additional stocks", Toast.LENGTH_SHORT).show()
+                dialog?.dismiss()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/arifwidayana/challengechapter4/view/homepage/EditStocksFragment.kt
@@ -0,0 +1,100 @@
+package com.arifwidayana.challengechapter4.view.homepage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.arifwidayana.challengechapter4.databinding.FragmentEditStocksBinding
+import com.arifwidayana.challengechapter4.model.StocksDatabase
+import com.arifwidayana.challengechapter4.model.database.StocksEntity
+import com.arifwidayana.challengechapter4.model.utils.SharedPreference
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+
+@DelicateCoroutinesApi
+class EditStocksFragment() : DialogFragment() {
+    private var bind : FragmentEditStocksBinding? = null
+    private val binding get() = bind!!
+    private var dataStocks : StocksDatabase? = null
+    private lateinit var shared : SharedPreference
+    lateinit var listStock : StocksEntity
+
+    constructor(liststocks: StocksEntity): this() {
+        this.listStock = liststocks
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        bind = FragmentEditStocksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        shared = SharedPreference(requireContext())
+        dataStocks = StocksDatabase.getData(requireContext())
+
+
+        if (this::listStock.isInitialized) {
+            binding.apply {
+                etEditCodeStocks.setText(listStock.codeStock.toString())
+                etEditNameStocks.setText(listStock.nameStock.toString())
+                etEditEquityStocks.setText(listStock.valueEquity.toString())
+                etEditNetProfitStocks.setText(listStock.valueNetProfit.toString())
+                etPbvStocks.setText(listStock.priceBookValue.toString())
+                etEpsStocks.setText(listStock.earningsPerShare.toString())
+                etEditPriceShareStocks.setText(listStock.sharePrice.toString())
+                etEditShareStocks.setText(listStock.shareStock.toString())
+            }
+        }
+        binding.apply {
+            btnEditStocks.setOnClickListener {
+                val codeStocks =  etEditCodeStocks.text.toString()
+                val nameStocks = etEditNameStocks.text.toString()
+                val equityStocks = etEditEquityStocks.text.toString().toInt()
+                val netProfitStocks = etEditNetProfitStocks.text.toString().toInt()
+                val priceStocks = etEditPriceShareStocks.text.toString().toInt()
+                val sharesStocks = etEditShareStocks.text.toString().toInt()
+                val bvStocks = equityStocks/sharesStocks
+                val pbvStocks = priceStocks/bvStocks.toDouble()
+                val epsStocks = netProfitStocks.toDouble()/sharesStocks
+                listStock.apply {
+                    codeStock = codeStocks
+                    nameStock = nameStocks
+                    valueEquity = equityStocks
+                    valueNetProfit= netProfitStocks
+                    sharePrice = priceStocks
+                    shareStock = sharesStocks
+                    priceBookValue = pbvStocks
+                    earningsPerShare = epsStocks
+                }
+                GlobalScope.async {
+                    dataStocks?.stocksDao()?.updateDataStocks(listStock)
+                }
+                Toast.makeText(requireContext(), "Edit Stocks Success", Toast.LENGTH_SHORT).show()
+                dialog?.dismiss()
+            }
+
+            btnDelete.setOnClickListener {
+                GlobalScope.async {
+                    dataStocks?.stocksDao()?.deleteDataStocks(listStock)
+                }
+                Toast.makeText(requireContext(), "Delete Stocks Success", Toast.LENGTH_SHORT).show()
+                dialog?.dismiss()
+            }
+        }
+    }
+
+}
