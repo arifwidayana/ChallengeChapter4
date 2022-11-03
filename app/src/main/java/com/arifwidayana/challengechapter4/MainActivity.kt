9af5aef2d4f6e05fe946d4ce930d40f3fbad547b
Arif Widayana <arifwidayana@gmail.com>
2022-11-03 16:16:34 +0700

    add hilt in application

--- app/src/main/java/com/arifwidayana/challengechapter4/MainActivity.kt
+++ app/src/main/java/com/arifwidayana/challengechapter4/MainActivity.kt
@@ -3,7 +3,9 @@ package com.arifwidayana.challengechapter4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.arifwidayana.challengechapter4.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
