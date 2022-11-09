c37dc84949552f13b906cba900f68ab311392193
Arif Widayana <arifwidayana@gmail.com>
2022-11-09 17:57:21 +0700

    add common resource

---
+++ app/src/main/java/com/arifwidayana/challengechapter4/common/Resource.kt
@@ -0,0 +1,14 @@
+package com.arifwidayana.challengechapter4.common
+
+import java.lang.Exception
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null,
+    val exception: Exception? = null
+) {
+    class Success<T>(data: T? = null, message: String? = null): Resource<T>(data, message)
+    class Loading<T>(data: T? = null): Resource<T>(data)
+    class Empty<T>(data: T? = null): Resource<T>(data)
+    class Error<T>(exception: Exception? = null, data: T? = null): Resource<T>(data, exception = exception)
+}
