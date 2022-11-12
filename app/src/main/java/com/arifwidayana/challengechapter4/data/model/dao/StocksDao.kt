44985886bd81602e1caceda5007376df43ede4ce
Arif Widayana <arifwidayana@gmail.com>
2022-11-12 21:40:46 +0700

    add function get list detail stocks by id

--- app/src/main/java/com/arifwidayana/challengechapter4/data/model/dao/StocksDao.kt
+++ app/src/main/java/com/arifwidayana/challengechapter4/data/model/dao/StocksDao.kt
@@ -8,14 +8,17 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface StocksDao {
     @Query("SELECT * FROM stock_table WHERE user_stock = :username")
-    fun getStocks(username: String): Flow<List<StocksEntity>>
+    fun getStocks(username: String): Flow<List<StocksEntity?>>
+
+    @Query("SELECT * FROM stock_table WHERE id = :idStocks")
+    fun getStocksById(idStocks: Int): Flow<StocksEntity?>
 
     @Insert(onConflict = REPLACE)
-    suspend fun insertStocks(stocks: StocksEntity)
+    suspend fun insertStocks(stocks: StocksEntity?)
 
     @Update
-    suspend fun updateStocks(stocks: StocksEntity)
+    suspend fun updateStocks(stocks: StocksEntity?)
 
-    @Query("DELETE FROM stock_table WHERE id = :id")
-    suspend fun deleteStocks(id: Int)
+    @Query("DELETE FROM stock_table WHERE id = :idStocks")
+    suspend fun deleteStocks(idStocks: Int)
 }
