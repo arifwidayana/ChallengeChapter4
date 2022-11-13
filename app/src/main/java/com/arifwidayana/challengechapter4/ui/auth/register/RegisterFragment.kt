4ff4349dd8390f49712a50d1b8c56755df1cb0fa
Arif Widayana <arifwidayana@gmail.com>
2022-11-13 23:11:56 +0700

    clear unused and boiler code

--- app/src/main/java/com/arifwidayana/challengechapter4/ui/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/arifwidayana/challengechapter4/ui/auth/register/RegisterFragment.kt
@@ -1,7 +1,10 @@
 package com.arifwidayana.challengechapter4.ui.auth.register
 
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.arifwidayana.challengechapter4.R
 import com.arifwidayana.challengechapter4.common.Resource
 import com.arifwidayana.challengechapter4.common.base.BaseFragment
 import com.arifwidayana.challengechapter4.data.model.request.RegisterRequest
@@ -13,20 +16,17 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding, RegisterViewModel
     FragmentRegisterBinding::inflate
 ) {
     override fun initView() {
-        onView()
         onClick()
-    }
-
-    private fun onView() {
-        binding.apply {
-            // do nothing
-        }
     }
 
     private fun onClick() {
         binding.apply {
             btnRegister.setOnClickListener {
                 registerUser()
+            }
+            requireActivity().onBackPressedDispatcher.addCallback {
+                handleOnBackPressed()
+                findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
             }
         }
     }
@@ -46,110 +46,13 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding, RegisterViewModel
     override fun observeData() {
         lifecycleScope.launchWhenStarted {
             viewModelInstance.registerResult.collect {
-                when(it) {
-                    is Resource.Loading -> {}
-                    is Resource.Empty -> {}
-                    is Resource.Success -> {
-                        showError(true, it.message.toString())
-                        findNavController().popBackStack()
-                    }
-                    is Resource.Error -> {
-                        showMessage(true, it.message.toString())
-                    }
+                if (it is Resource.Success) {
+                    showError(true, it.message.toString())
+                    findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+                } else if (it is Resource.Error) {
+                    showMessage(true, it.message.toString())
                 }
             }
         }
     }
-    //        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        dataUser = StocksDatabase.getInstance(requireContext())
-//
-//        binding.apply {
-//            etRegisterPassword.addTextChangedListener(object : TextWatcher{
-//                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-//
-//                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                    when {
-//                        etRegisterPassword.text.toString().length <= 8 -> {
-//                            etRegisterPassword.error = "At least 8 words password"
-//                            etConfirmPassword.error = null
-//                        }
-//                        etConfirmPassword.text.toString().length <= 8 -> {
-//                            etConfirmPassword.error = "At least 8 words confirm password"
-//                            etRegisterPassword.error = null
-//                        }
-//                        else -> {
-//                            etRegisterPassword.error = null
-//                            etConfirmPassword.error = null
-//                        }
-//                    }
-//                }
-//
-//                override fun afterTextChanged(p0: Editable?) {}
-//
-//            })
-//
-//            etConfirmPassword.addTextChangedListener(object : TextWatcher{
-//                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-//
-//                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                    when {
-//                        etRegisterPassword.text.toString().length <= 8 -> {
-//                            etRegisterPassword.error = "At least 8 words password"
-//                            etConfirmPassword.error = null
-//                        }
-//                        etConfirmPassword.text.toString().length <= 8 -> {
-//                            etConfirmPassword.error = "At least 8 words confirm password"
-//                            etRegisterPassword.error = null
-//                        }
-//                        else -> {
-//                            etRegisterPassword.error = null
-//                            etConfirmPassword.error = null
-//                        }
-//                    }
-//                }
-//
-//                override fun afterTextChanged(p0: Editable?) {}
-//            })
-//        }
-//
-//        binding.apply {
-//            btnRegister.setOnClickListener {
-//                val objDataUser = UserEntity(null, etRegisterName.text.toString(), etRegisterUsername.text.toString(), etConfirmPassword.text.toString())
-//
-//                when {
-//                    etRegisterName.text.isNullOrEmpty() -> {
-//                        tfName.error = "Fill column name"
-//                    }
-//                    etRegisterUsername.text.isNullOrEmpty() -> {
-//                        tfUsername.error = "Fill column username"
-//                    }
-//                    etRegisterPassword.text.isNullOrEmpty() -> {
-//                        tfPassword.error = "Fill column Password"
-//                    }
-//                    etConfirmPassword.text.isNullOrEmpty() -> {
-//                        tfConfPassword.error = "Fill column Confirm Password"
-//                    }
-//                    etConfirmPassword.text.toString() != etRegisterPassword.text.toString() -> {
-//                        Toast.makeText(requireContext(), "Password not Match", Toast.LENGTH_SHORT)
-//                            .show()
-//                    }
-//                    else -> {
-//                        GlobalScope.async {
-//                            dataUser?.userDao()?.insertUser(objDataUser)
-//                        }
-//                        CoroutineScope(Dispatchers.Main).launch {
-//                            Toast.makeText(
-//                                requireContext(),
-//                                "Register Success",
-//                                Toast.LENGTH_SHORT
-//                            ).show()
-//                            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
